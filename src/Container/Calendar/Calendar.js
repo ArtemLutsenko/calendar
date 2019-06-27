fc01f9352656557f59c7465b9ef7c8e4c508f7ff
ArtemLutsenko <artem.lutsenko@gmail.com>
2019-06-27 17:16:13 +0300

    first commit

---
+++ src/Container/Calendar/Calendar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import classes from './Calendar.css'
+
+class Calendar extends Component{
+    state ={
+
+    }
+
+    render(){
+        return(
+            <div className = {classes.Calendar}> Calendar</div>
+        )
+    }
+}
+
+export default Calendar
+
